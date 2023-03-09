15d8da4448953ba9e48936d692e321c346beffaf
Yulia <ybe2004ybe@gmail.com>
2023-03-09 21:07:23 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,30 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-        for (int count2 = 1; count2 <= 9; count2++) {
-            for (int count = 1; count <= 9; count++) {
-                System.out.println(count2 + " x " + count + " = " + (count * count2));
+//        for (int count = 1; count <= 9; count++) {
+//            for (int count2 = 1; count2 <= 9; count2++) {
+//                system.out.println(count + " x " + count2 + " = " + (count * count2));
+//                if (count == 9) {
+//                    system.out.println();
+//                }
+//
+//            }
+//            system.out.println();
+//        }
 
+        for (int j = 1; j <= 9; j++) {
+            for (int i = 1; i <= 9; i++) {
+                int sum = j * i;
+                System.out.print(i + " x " + j + " = " + sum + "   ");
+                if (sum < 10) {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
     }
 }
+
 
 // до 60 в городе
 // до 90 населенные пункты
